65225caa42085bf7a7427f77831a8a1271f8dc9b
Ian Speelman <ianspeelman@gmail.com>
2024-10-28 15:26:57 +0100

    Remove git cache
    this makes sure the node_modules folder is removed from the repo

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var port = 3001;
-var app = (0, express_1.default)();
-app.listen(port, function () { return console.log("Server is listening on port ".concat(port)); });
