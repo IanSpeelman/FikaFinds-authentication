bb234f4eb66acc86827110b61dece1c859d04e7e
Ian Speelman <ianspeelman@gmail.com>
2024-10-29 15:24:50 +0100

    Add User model

---
+++ src/models/userModel.ts
@@ -0,0 +1,38 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../utils/database";
+
+class User extends Model { }
+
+User.init({
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    admin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+},
+    {
+        sequelize,
+        modelName: "User"
+    })
+
+async function syncTable() {
+    await User.sync({ alter: true })
+}
+syncTable()
+
+export default User
