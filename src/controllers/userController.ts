f4ff5ffd7f081582bf4e559fc3e9b2de11858e88
Ian Speelman <ianspeelman@gmail.com>
2024-10-30 11:03:17 +0100

    password hashing and jwt tokens
    hashing passwords when storing in the database
    checking password against hash when logging in
    generating a jwt and set the response authorization header to that token
    creating a (temporary) check route to validate the jwt

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express'
 import checkValidData from '../utils/checkValidData'
 import User from '../models/userModel'
-import { UserType } from '../utils/types'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+const jwtSecret = process.env.JWTSECRET ? process.env.JWTSECRET : "supersecure"
 
 export async function login(req: Request, res: Response) {
   const { email, password } = req.body
@@ -10,6 +11,9 @@ export async function login(req: Request, res: Response) {
   if (user) {
     const passwordMatch = await bcrypt.compare(password, user.password)
     if (passwordMatch) {
+      const jwtPayload = { email, admin: user.admin, firstName: user.firstName }
+      const token = jwt.sign(jwtPayload, jwtSecret)
+      res.set('Authorization', token)
       res.json({ status: "logging in success" }).end()
     }
     else {
@@ -24,11 +28,34 @@ export async function register(req: Request, res: Response) {
       bcrypt.hash(user.password, 1, async (err, password) => {
         await User.create({ ...user, password, admin: false })
       })
+      const jwtPayload = { email: user.email, admin: false, firstName: user.firstName }
+      const token = jwt.sign(jwtPayload, jwtSecret)
+      res.set('Authorization', token).end()
 
     } catch (err) {
       console.log("something went wrong saving this user", err)
+      res.status(500).json({ err: "something went wrong" })
     }
   }
+  else {
+    res.status(406).json({ err: "body does not meet requirements" })
+  }
+}
 
-  res.json(user)
+export function checkJWT(req: Request, res: Response) {
+  const header = req.get('Authorization')?.split(" ")[1]
+  if (header) {
+    try {
+      const result = jwt.verify(header, jwtSecret);
+      res.json({ result })
+    }
+    catch (err) {
+      console.log(err)
+      res.json({ result: "failed" })
+    }
+  }
+  else {
+    res.json({ result: "failed" })
+  }
+
 }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login, register } from "../controllers/userController";
+import { login, register, checkJWT } from "../controllers/userController";
 
 const router = Router();
 
 router.post('/login', login)
 router.post('/register', register)
+router.get('/check', checkJWT)
 
 export default router
