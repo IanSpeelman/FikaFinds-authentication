62d26cee693ea4947d1ebba6b40f312d945b8bea
Ian Speelman <ianspeelman@gmail.com>
2024-10-30 09:26:08 +0100

    add login logic
    login logic with encryption for password in place, just on the backend.
    no tokens or confirmation apart from a json response are sent back to
    the client

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,19 +1,34 @@
 import { Request, Response } from 'express'
 import checkValidData from '../utils/checkValidData'
 import User from '../models/userModel'
+import { UserType } from '../utils/types'
+import bcrypt from 'bcrypt'
 
-export function login(req: Request, res: Response) {
-    res.json(req.body)
+export async function login(req: Request, res: Response) {
+  const { email, password } = req.body
+  const user = await User.findOne({ where: { email: email } })
+  if (user) {
+    const passwordMatch = await bcrypt.compare(password, user.password)
+    if (passwordMatch) {
+      res.json({ status: "logging in success" }).end()
+    }
+    else {
+      res.json({ status: "username and password combination do not match" }).end()
+    }
+  }
 }
 export async function register(req: Request, res: Response) {
-    const user = checkValidData(req.body)
-    if (user) {
-        try {
-            await User.create({ ...user, admin: false })
-        } catch (err) {
-            console.log("something went wrong saving this user", err)
-        }
-    }
+  const user = checkValidData(req.body)
+  if (user) {
+    try {
+      bcrypt.hash(user.password, 1, async (err, password) => {
+        await User.create({ ...user, password, admin: false })
+      })
 
-    res.json(user)
+    } catch (err) {
+      console.log("something went wrong saving this user", err)
+    }
+  }
+
+  res.json(user)
 }
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,37 +1,44 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../utils/database";
+import { UserType } from "../utils/types";
 
-class User extends Model { }
+class User extends Model<UserType> {
+  public firstName!: string;
+  public lastName!: string;
+  public email!: string;
+  public password!: string;
+  public admin!: boolean;
+}
 
 User.init({
-    firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    lastName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    admin: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false
-    }
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  admin: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  }
 },
-    {
-        sequelize,
-        modelName: "User"
-    })
+  {
+    sequelize,
+    modelName: "User"
+  })
 
 async function syncTable(): Promise<void> {
-    await User.sync({ alter: true })
+  await User.sync({ alter: true })
 }
 syncTable()
 
