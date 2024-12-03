6f54f0fc5fab6e48aa1d64290a01eef1560cbcc3
Ian Speelman <ianspeelman@gmail.com>
2024-12-03 16:14:17 +0100

    users can now order after registering
    previously the user had to log out and back in to be able to place an
    order. this is now fixed

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,6 +5,8 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { UserType } from '../utils/types'
 const jwtSecret = process.env.JWTSECRET ? process.env.JWTSECRET : "supersecure"
+const jwtExp = 3600;
+
 
 export async function login(req: Request, res: Response) {
     const { email, password } = req.body
@@ -12,7 +14,7 @@ export async function login(req: Request, res: Response) {
     if (user) {
         const passwordMatch = await bcrypt.compare(password, user.password)
         if (passwordMatch) {
-            const jwtPayload = { id: user.id, email, admin: user.admin, firstName: user.firstName }
+            const jwtPayload = { id: user.id, email, admin: user.admin, firstName: user.firstName, exp: Math.floor(Date.now() / 1000) + jwtExp }
             const token = jwt.sign(jwtPayload, jwtSecret)
             res.set("Access-Control-Expose-Headers", "Authorization")
             res.set('Authorization', token)
@@ -34,10 +36,14 @@ export async function register(req: Request, res: Response) {
             bcrypt.hash(user.password, 1, async (err, password) => {
                 await User.create({ ...user, password, admin: false })
             })
-            const jwtPayload = { email: user.email, admin: false, firstName: user.firstName }
-            const token = jwt.sign(jwtPayload, jwtSecret)
-            res.set("Access-Control-Expose-Headers", "Authorization")
-            res.status(201).set('Authorization', token).end()
+            const newUser: UserType | null = await User.findOne({ where: { email: user.email } })
+            if (newUser) {
+                const jwtPayload = { id: newUser.id, email: user.email, admin: user.admin, firstName: user.firstName, exp: Math.floor(Date.now() / 1000) + jwtExp }
+                const token = jwt.sign(jwtPayload, jwtSecret)
+                res.set("Access-Control-Expose-Headers", "Authorization")
+                res.set('Authorization', token)
+                res.status(201).set('Authorization', token).end()
+            }
 
         } catch (err) {
             console.log("something went wrong saving this user", err)
